a546ca530d91961d6bf62148bf1b9acf2f040406
oleggorkov <ole.gorkov@yandex.ru>
2019-09-05 14:09:53 +0300

    master

---
+++ frontend/views/site/task-tracker.php
@@ -0,0 +1,20 @@
+<?php
+
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+$this->title = 'Task tracker';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-taskTracker">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <code>Задачи</code>
+    <code>Новая задача</code>
+    <code>Отслеживаемые задачи</code>
+    <code>Календарь</code>
+    <code>Задачи</code>
+    <code>Участники</code>
+    <code>Документы</code>
+    <code>Настройки</code>
+
+</div>
