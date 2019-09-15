c1e9f3695163bb2e8c850227d1a2b5209a993e58
oleggorkov <ole.gorkov@yandex.ru>
2019-09-15 22:07:37 +0300

    lesson3

--- backend/config/main.php
+++ backend/config/main.php
@@ -5,21 +5,14 @@ $params = array_merge(
     require __DIR__ . '/params.php',
     require __DIR__ . '/params-local.php'
 );
-
-return [
+$config = [
     'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
+//    'defaultRouter' => 'project/index',
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
-            'view' => [
-                'theme' => [
-                     'pathMap' => [
-                             '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
-        ],
-    ],
-],
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
@@ -44,14 +37,29 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][] = 'log';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+}
+return $config;
